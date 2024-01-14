e4b1267970adfe98fde14e5c696d2160c67d7a3a
zhiyan114 <zhiyan114@zhiyan114.com>
2024-01-14 15:41:57 -0500

    MQTT using non-URI format due to it being unsupported

--- DevTool/Form1.Designer.cs
+++ DevTool/Form1.Designer.cs
@@ -31,7 +31,7 @@
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
-            mqttUri = new TextBox();
+            mqttIP = new TextBox();
             mqttLogin = new Button();
             mqttLogout = new Button();
             label4 = new Label();
@@ -50,12 +50,19 @@
             timeTS = new DateTimePicker();
             label10 = new Label();
             Identifier = new TextBox();
+            label11 = new Label();
+            mqttPort = new TextBox();
+            mqttTLS = new CheckBox();
+            label12 = new Label();
+            label13 = new Label();
+            mqttUsername = new TextBox();
+            mqttPassword = new TextBox();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(-7, 106);
+            label1.Location = new Point(-7, 171);
             label1.Name = "label1";
             label1.Size = new Size(497, 15);
             label1.TabIndex = 0;
@@ -77,33 +84,34 @@
             label3.AutoSize = true;
             label3.Location = new Point(12, 49);
             label3.Name = "label3";
-            label3.Size = new Size(62, 15);
+            label3.Size = new Size(42, 15);
             label3.TabIndex = 2;
-            label3.Text = "MQTT URI:";
+            label3.Text = "IP/DN:";
             // 
-            // mqttUri
+            // mqttIP
             // 
-            mqttUri.Location = new Point(80, 46);
-            mqttUri.Name = "mqttUri";
-            mqttUri.Size = new Size(393, 23);
-            mqttUri.TabIndex = 3;
+            mqttIP.Location = new Point(60, 46);
+            mqttIP.Name = "mqttIP";
+            mqttIP.Size = new Size(270, 23);
+            mqttIP.TabIndex = 1;
             // 
             // mqttLogin
             // 
-            mqttLogin.Location = new Point(12, 89);
+            mqttLogin.Location = new Point(12, 145);
             mqttLogin.Name = "mqttLogin";
             mqttLogin.Size = new Size(75, 23);
-            mqttLogin.TabIndex = 4;
+            mqttLogin.TabIndex = 6;
             mqttLogin.Text = "Login";
             mqttLogin.UseVisualStyleBackColor = true;
+            mqttLogin.Click += mqttLogin_Click;
             // 
             // mqttLogout
             // 
             mqttLogout.Enabled = false;
-            mqttLogout.Location = new Point(93, 89);
+            mqttLogout.Location = new Point(93, 145);
             mqttLogout.Name = "mqttLogout";
             mqttLogout.Size = new Size(75, 23);
-            mqttLogout.TabIndex = 5;
+            mqttLogout.TabIndex = 7;
             mqttLogout.Text = "Logout";
             mqttLogout.UseVisualStyleBackColor = true;
             // 
@@ -112,7 +120,7 @@
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 15.75F, FontStyle.Underline, GraphicsUnit.Point, 0);
             label4.ForeColor = Color.FromArgb(0, 192, 0);
-            label4.Location = new Point(126, 128);
+            label4.Location = new Point(126, 193);
             label4.Name = "label4";
             label4.Size = new Size(220, 30);
             label4.TabIndex = 6;
@@ -122,7 +130,7 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label5.Location = new Point(246, 91);
+            label5.Location = new Point(246, 144);
             label5.Name = "label5";
             label5.Size = new Size(100, 21);
             label5.TabIndex = 7;
@@ -133,7 +141,7 @@
             mqttStatus.AutoSize = true;
             mqttStatus.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             mqttStatus.ForeColor = Color.Red;
-            mqttStatus.Location = new Point(340, 91);
+            mqttStatus.Location = new Point(340, 144);
             mqttStatus.Name = "mqttStatus";
             mqttStatus.Size = new Size(61, 21);
             mqttStatus.TabIndex = 8;
@@ -142,7 +150,7 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(12, 169);
+            label6.Location = new Point(12, 234);
             label6.Name = "label6";
             label6.Size = new Size(84, 15);
             label6.TabIndex = 9;
@@ -151,7 +159,7 @@
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(12, 198);
+            label7.Location = new Point(12, 263);
             label7.Name = "label7";
             label7.Size = new Size(76, 15);
             label7.TabIndex = 10;
@@ -160,7 +168,7 @@
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(14, 227);
+            label8.Location = new Point(14, 292);
             label8.Name = "label8";
             label8.Size = new Size(60, 15);
             label8.TabIndex = 11;
@@ -168,32 +176,32 @@
             // 
             // pubName
             // 
-            pubName.Location = new Point(102, 166);
+            pubName.Location = new Point(102, 231);
             pubName.Name = "pubName";
             pubName.Size = new Size(371, 23);
-            pubName.TabIndex = 12;
+            pubName.TabIndex = 8;
             // 
             // Temperature
             // 
-            Temperature.Location = new Point(102, 195);
+            Temperature.Location = new Point(102, 260);
             Temperature.Name = "Temperature";
             Temperature.Size = new Size(371, 23);
-            Temperature.TabIndex = 13;
+            Temperature.TabIndex = 9;
             // 
             // Humidity
             // 
-            Humidity.Location = new Point(102, 224);
+            Humidity.Location = new Point(102, 289);
             Humidity.Name = "Humidity";
             Humidity.Size = new Size(371, 23);
-            Humidity.TabIndex = 14;
+            Humidity.TabIndex = 10;
             // 
             // sendData
             // 
             sendData.Enabled = false;
-            sendData.Location = new Point(12, 330);
+            sendData.Location = new Point(12, 395);
             sendData.Name = "sendData";
             sendData.Size = new Size(331, 23);
-            sendData.TabIndex = 15;
+            sendData.TabIndex = 14;
             sendData.Text = "Send Sample Data";
             sendData.UseVisualStyleBackColor = true;
             // 
@@ -201,10 +209,10 @@
             // 
             overrideTS.AutoSize = true;
             overrideTS.Enabled = false;
-            overrideTS.Location = new Point(349, 333);
+            overrideTS.Location = new Point(349, 398);
             overrideTS.Name = "overrideTS";
             overrideTS.Size = new Size(133, 19);
-            overrideTS.TabIndex = 16;
+            overrideTS.TabIndex = 15;
             overrideTS.Text = "Override Timestamp";
             overrideTS.UseVisualStyleBackColor = true;
             overrideTS.CheckedChanged += overrideTS_CheckedChanged;
@@ -212,7 +220,7 @@
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(14, 285);
+            label9.Location = new Point(14, 350);
             label9.Name = "label9";
             label9.Size = new Size(69, 15);
             label9.TabIndex = 17;
@@ -221,25 +229,25 @@
             // dateTS
             // 
             dateTS.Enabled = false;
-            dateTS.Location = new Point(102, 282);
+            dateTS.Location = new Point(102, 347);
             dateTS.Name = "dateTS";
             dateTS.Size = new Size(223, 23);
-            dateTS.TabIndex = 18;
+            dateTS.TabIndex = 12;
             // 
             // timeTS
             // 
             timeTS.Enabled = false;
             timeTS.Format = DateTimePickerFormat.Time;
-            timeTS.Location = new Point(331, 282);
+            timeTS.Location = new Point(331, 347);
             timeTS.Name = "timeTS";
             timeTS.ShowUpDown = true;
             timeTS.Size = new Size(142, 23);
-            timeTS.TabIndex = 19;
+            timeTS.TabIndex = 13;
             // 
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(14, 256);
+            label10.Location = new Point(14, 321);
             label10.Name = "label10";
             label10.Size = new Size(57, 15);
             label10.TabIndex = 20;
@@ -247,16 +255,83 @@
             // 
             // Identifier
             // 
-            Identifier.Location = new Point(102, 253);
+            Identifier.Location = new Point(102, 318);
             Identifier.Name = "Identifier";
             Identifier.Size = new Size(371, 23);
-            Identifier.TabIndex = 21;
+            Identifier.TabIndex = 11;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(335, 49);
+            label11.Name = "label11";
+            label11.Size = new Size(32, 15);
+            label11.TabIndex = 22;
+            label11.Text = "Port:";
+            // 
+            // mqttPort
+            // 
+            mqttPort.Location = new Point(373, 46);
+            mqttPort.Name = "mqttPort";
+            mqttPort.Size = new Size(100, 23);
+            mqttPort.TabIndex = 2;
+            mqttPort.Text = "1883";
+            // 
+            // mqttTLS
+            // 
+            mqttTLS.AutoSize = true;
+            mqttTLS.Location = new Point(174, 148);
+            mqttTLS.Name = "mqttTLS";
+            mqttTLS.Size = new Size(66, 19);
+            mqttTLS.TabIndex = 5;
+            mqttTLS.Text = "Use TLS";
+            mqttTLS.UseVisualStyleBackColor = true;
+            mqttTLS.CheckedChanged += mqttTLS_CheckedChanged;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(11, 78);
+            label12.Name = "label12";
+            label12.Size = new Size(63, 15);
+            label12.TabIndex = 25;
+            label12.Text = "Username:";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(11, 107);
+            label13.Name = "label13";
+            label13.Size = new Size(60, 15);
+            label13.TabIndex = 26;
+            label13.Text = "Password:";
+            // 
+            // mqttUsername
+            // 
+            mqttUsername.Location = new Point(81, 75);
+            mqttUsername.Name = "mqttUsername";
+            mqttUsername.Size = new Size(392, 23);
+            mqttUsername.TabIndex = 3;
+            // 
+            // mqttPassword
+            // 
+            mqttPassword.Location = new Point(81, 104);
+            mqttPassword.Name = "mqttPassword";
+            mqttPassword.Size = new Size(392, 23);
+            mqttPassword.TabIndex = 4;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(485, 365);
+            ClientSize = new Size(485, 430);
+            Controls.Add(mqttPassword);
+            Controls.Add(mqttUsername);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(mqttTLS);
+            Controls.Add(mqttPort);
+            Controls.Add(label11);
             Controls.Add(Identifier);
             Controls.Add(label10);
             Controls.Add(timeTS);
@@ -275,7 +350,7 @@
             Controls.Add(label4);
             Controls.Add(mqttLogout);
             Controls.Add(mqttLogin);
-            Controls.Add(mqttUri);
+            Controls.Add(mqttIP);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -293,7 +368,7 @@
         private Label label1;
         private Label label2;
         private Label label3;
-        private TextBox mqttUri;
+        private TextBox mqttIP;
         private Button mqttLogin;
         private Button mqttLogout;
         private Label label4;
@@ -312,5 +387,12 @@
         private DateTimePicker timeTS;
         private Label label10;
         private TextBox Identifier;
+        private Label label11;
+        private TextBox mqttPort;
+        private CheckBox mqttTLS;
+        private Label label12;
+        private Label label13;
+        private TextBox mqttUsername;
+        private TextBox mqttPassword;
     }
 }
--- DevTool/Form1.cs
+++ DevTool/Form1.cs
@@ -1,7 +1,12 @@
+using MQTTnet;
+using MQTTnet.Client;
+
 namespace DevTool
 {
     public partial class Form1 : Form
     {
+        MqttFactory factory = new MqttFactory();
+        IMqttClient client;
         public Form1()
         {
             InitializeComponent();
@@ -11,6 +16,26 @@ namespace DevTool
         {
             dateTS.Enabled = overrideTS.Checked;
             timeTS.Enabled = overrideTS.Checked;
+        }
+
+        [Obsolete]
+        private void mqttLogin_Click(object sender, EventArgs e)
+        {
+            client = factory.CreateMqttClient();
+
+            string clientId = Guid.NewGuid().ToString();
+            var test = new MqttClientOptionsBuilder()
+                .WithClientId("DevTool_" + clientId)
+                .WithConnectionUri(new Uri(mqttIP.Text))
+                .Build();
+        }
+
+        private void mqttTLS_CheckedChanged(object sender, EventArgs e)
+        {
+            if(mqttPort.Text == "1883" && mqttTLS.Checked)
+                mqttPort.Text = "8883";
+            else if(mqttPort.Text == "8883" && !mqttTLS.Checked)
+                mqttPort.Text = "1883";
         }
     }
 }
